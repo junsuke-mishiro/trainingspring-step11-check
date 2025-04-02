2dcd1af46bcb26513261335d8c6b3e8d7b10c586
chari <chari@ASUSmishiro>
2025-04-02 17:26:40 +0900

    a

---
+++ trainingspringb-step11-check/src/main/java/com/example/demo3/TrainingspringbStep11CheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingspringbStep11CheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingspringbStep11CheckApplication.class, args);
+	}
+
+}
---
+++ trainingspringb-step11-check/src/main/java/com/example/demo3/controller/BaseController.java
@@ -0,0 +1,29 @@
+package com.example.demo3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo3.model.Person;
+
+@Controller
+public class BaseController{
+
+    @GetMapping("/")
+    public String home(@ModelAttribute Person person) {
+      return "form";
+    }
+
+    @PostMapping("/form")
+    public String result(@Validated
+                        @ModelAttribute Person person,
+                        BindingResult result){
+        if(result.hasErrors()){
+            return "form";
+        }
+        return "result";
+    }
+}
